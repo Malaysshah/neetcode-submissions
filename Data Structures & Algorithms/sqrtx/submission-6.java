3642d2abaf70ed9bc64778b2845668151f9fcb31
Malaysshah <malayshah94@gmail.com>
2026-06-30 09:06:10 +0530

    Add: sqrtx - submission-6

---
+++ Data Structures & Algorithms/sqrtx/submission-6.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x<=1)
+            return x;
+        int l=1;
+        int r=x/2;
+        int ans=0;
+        while(l<=r)
+        {
+            int mid=l+(r-l)/2;
+            if((long)mid*mid==x)
+            {
+                return (int)mid;
+            }
+            else if((long)mid*mid>x)
+            {
+                r=mid-1;
+            }
+            else
+            {
+                ans=mid;
+                l=mid+1;
+            }
+        }
+        return ans;
+    }
+}
